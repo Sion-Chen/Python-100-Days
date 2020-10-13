edfe5ae00232df601e07bf510a40ab0b1d2f1b19
着魔村支书陈二狗 <45960571+Sion-Chen@users.noreply.github.com>
2020-10-13 10:42:45 +0800

    Update for6.py

--- Day01-15/code/Day04/for6.py
+++ Day01-15/code/Day04/for6.py
@@ -44,3 +44,22 @@ for i in range(row):
     for _ in range(2 * i + 1):
         print('*', end='')
     print()
+   
+   
+   
+   """打印三角形"""
+line = 5
+for i in range(1, line + 1):
+    print('* ' * i)
+print()
+
+for i in range(1, line + 1):
+    print((line - i) * ' ' + '*' * i)
+print()
+
+# for i in range(1, line + 1):
+#     print(' ' * (line - i) + '* ' * i)
+# print()
+
+for i in range(line):
+    print(' ' * (line - i) + '*' * (2 * i + 1))
